0657681ca05e039e7c6203e72b1e2ed7e74c6460
carolinabois <carobois.7@gmail.com>
2020-09-27 20:58:24 -0300

    cambios

--- proyectos.php
+++ proyectos.php
@@ -115,29 +115,7 @@ $pg = "proyectos" ;
                 </div>
             </div>
     </main>
-    <footer>
-        <div class="container">
-            <div class="row mb-2 mt-4 mb-sm-5">
-                <div class="col-12 col-sm text-sm-left text-center pb-sm-0 pb-2">
-                    <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/carolinabois" target="_blank" title="Linkedin"><i
-                            class="fab fa-linkedin-in"></i></a>
-                    <a href="https://api.Whatsapp.com/send?phone=5492345411692" target="_blank" title="Whatsapp"><i
-                            class="fab fa-whatsapp"></i></a>
-                    <a href="mailto:carobois.7@gmail.com" target="_blank" title="Email"><i
-                            class="far fa-envelope"></i></a>
-                </div>
-                <div class="col-12 col-sm text-sm-center text-center">
-                    <a href="mailto:carobois.7@gmail.com">info@carolinabois.com.ar</a>
-                </div>
-                <div class="col-12 col-sm text-sm-right text-center">
-                    Sponsor <a href="https://depsuite.com" target="_blank" title="DEPCSUITE">DePC Suite</a>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
+    <?php include_once ("footer.php") ; ?>
 </body>
 
 </html>
--- sobre-mi.php
+++ sobre-mi.php
@@ -253,29 +253,7 @@ $pg = "sobre-mi" ;
         </section>
 
     </main>
-    <footer>
-        <div class="container">
-            <div class="row mb-2 mt-5">
-                <div class="col-12 col-sm text-sm-left text-center ">
-                    <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/carolinabois" target="_blank" title="Linkedin"><i
-                            class="fab fa-linkedin-in"></i></a>
-                    <a href="https://api.Whatsapp.com/send?phone=5492345411692" target="_blank" title="Whatsapp"><i
-                            class="fab fa-whatsapp"></i></a>
-                    <a href="mailto:carobois.7@gmail.com" target="_blank" title="Email"><i
-                            class="far fa-envelope"></i></a>
-                </div>
-                <div class="col-12 col-sm text-sm-center text-center">
-                    <a href="mailto:carobois.7@gmail.com">info@carolinabois.com.ar</a>
-                </div>
-                <div class="col-12 col-sm text-sm-right text-center">
-                    Sponsor <a href="https://depsuite.com" target="_blank" title="DEPCSUITE">DePC Suite</a>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
+    <?php include_once ("footer.php") ; ?>
 </body>
 
 </html>
